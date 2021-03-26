6ae4f171da83538fdce513119b5a27016c0936d1
yanivcogan <yanivcogan89@gmail.com>
2021-03-26 19:48:51 +0300

    code formatting

--- Qr.c
+++ Qr.c
@@ -8,29 +8,28 @@
 #define EPSILON 0.0001
 
 /*identity matrix*/
-double** get_identity(int n){
-    int i,j;
-    double **res=my_alloc(n,n);
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            res[i][j]=(i==j)?1:0;
+double **get_scalar_matrix(int n, int k) {
+    int i, j;
+    double **res = my_alloc(n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            res[i][j] = (i == j) ? k : 0;
         }
     }
-
     return res;
 }
 
 /*check difference smaller than EPSILON*/
-int epsilon_diff(double **first,double **second,int n){
-    int i,j;
-    double pos_first,pos_sec,res;
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            pos_first=fabs(first[i][j]); pos_sec=fabs(second[i][j]);
-//printf("-- %lf --",(pos_first-pos_sec));
-            res=pos_first-pos_sec;
-            if(res>EPSILON || res<(-EPSILON) ){
-                printf("yey\n");
+int epsilon_diff(double **first, double **second, int n) {
+    int i, j;
+    double pos_first, pos_sec, res;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            pos_first = fabs(first[i][j]);
+            pos_sec = fabs(second[i][j]);
+            //printf("-- %lf --",(pos_first-pos_sec));
+            res = pos_first - pos_sec;
+            if (res > EPSILON || res < (-EPSILON)) {
                 return 1;
             }
         }
@@ -41,60 +40,55 @@ int epsilon_diff(double **first,double **second,int n){
 
 
 /*QR iteration*/
-double ***qr_iter(double** A,int n){
-    double **A_tag,**Q_tag,**update,**Q,**R;
-    double ***ret,***obtain_q_r;
-    int i,j,k;
-    A_tag=my_alloc(n,n);
-    update=my_alloc(n,n);
-    Q_tag=get_identity(n);
-    Q=my_alloc(n,n);R=my_alloc(n,n);
-    ret=(double***)malloc(2*sizeof(double **));
-    obtain_q_r=(double***)malloc(2*sizeof(double **));
-
-    assert(ret!=NULL);
-    copy_arrays(A_tag,A,n);
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            update[i][j]=0;
-
-
-        }
-    }
-
-    for(i=0;i<n;i++){
-        modifeied_gram_shmidt(A_tag,n,obtain_q_r);
-
-        copy_arrays(Q,obtain_q_r[0],n);copy_arrays(R,obtain_q_r[1],n);
-        free_arrays(obtain_q_r[0],n); free_arrays(obtain_q_r[1],n);
-
-        mult_matrices(R,Q,A_tag,n);
-
-        mult_matrices(Q_tag,Q,update,n);
-
-
-        if (!epsilon_diff(Q_tag,update,n)){
+double ***qr_iter(double **A, int n) {
+    /* variable declaration */
+    double **A_tag, **Q_tag, **updated_Q_tag, **Q, **R;
+    double ***ret, ***obtain_q_r;
+    int i, j;
+    Q = my_alloc(n, n);
+    R = my_alloc(n, n);
+    ret = (double ***) malloc(2 * sizeof(double **));
+    assert(ret != NULL);
+    obtain_q_r = (double ***) malloc(2 * sizeof(double **));
+    assert(obtain_q_r != NULL);
+    /* initiate A_tag */
+    A_tag = my_alloc(n, n);
+    copy_arrays(A_tag, A, n);
+    /* initiate Q_tag */
+    Q_tag = get_scalar_matrix(n, 1);
+    /* initiate updated_Q_tag */
+    updated_Q_tag = get_scalar_matrix(n, 0);
+    for (i = 0; i < n; i++) {
+        modified_gram_schmidt(A_tag, n, obtain_q_r);
+        /* move results from array to the Q, R variables */
+        copy_arrays(Q, obtain_q_r[0], n);
+        copy_arrays(R, obtain_q_r[1], n);
+        free_arrays(obtain_q_r[0], n);
+        free_arrays(obtain_q_r[1], n);
+        /* updated_Q_tag A_tag matrix to be RQ */
+        mult_matrices(R, Q, A_tag, n);
+        /* multiply Q_tag with Q, store in updated_Q_tag  */
+        mult_matrices(Q_tag, Q, updated_Q_tag, n);
+        /* check epsilon difference */
+        if (!epsilon_diff(Q_tag, updated_Q_tag, n)) {
             printf("finish");
-            ret[0]=A_tag;ret[1]=Q_tag;
-            free_arrays(update,n);
-            free_arrays(Q,n);free_arrays(R,n);
+            ret[0] = A_tag;
+            ret[1] = Q_tag;
+            free_arrays(updated_Q_tag, n);
+            free_arrays(Q, n);
+            free_arrays(R, n);
             free(obtain_q_r);
-
             return ret;
         }
-        printf("still run\n\n");
-
-
-
-        copy_arrays(Q_tag,update,n);
-
+        /* set Q_tag to updated_Q_tag */
+        copy_arrays(Q_tag, updated_Q_tag, n);
     }
-    printf("all iterations passed\n");
-    //need do not free if n=0 (possible?)
-    ret[0]=A_tag;ret[1]=Q_tag;
+    /*printf("all iterations passed\n");*/
+    ret[0] = A_tag;
+    ret[1] = Q_tag;
     free(obtain_q_r);
-
-    free_arrays(update,n);
-    free_arrays(Q,n);free_arrays(R,n);
+    free_arrays(updated_Q_tag, n);
+    free_arrays(Q, n);
+    free_arrays(R, n);
     return ret;
 }
