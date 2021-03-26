6b3d1c251153d757623c7fb434915713c6198bc7
yanivcogan <yanivcogan89@gmail.com>
2021-03-26 19:50:04 +0300

    c-api now returns values from QR iteration algorithm

--- EigenValsCPython.c
+++ EigenValsCPython.c
@@ -1,5 +1,5 @@
 #define PY_SSIZE_T_CLEAN
-#include "C:\Users\LENOVO\AppData\Local\Programs\Python\Python38\include\Python.h"
+#include "C:\Users\Yaniv\AppData\Local\Programs\Python\Python39\include\Python.h"
 #include "Qr.h"
 #include "ShmidtAux.h"
 #include "EigengapHeuristic.h"
@@ -10,52 +10,63 @@ static void err_message(char *err){
 }
 
 static void convert_float_double(double**data, PyObject *data_python) {
-
-    Py_ssize_t n,d,counter_n,counter_d;
+    Py_ssize_t n, d, counter_n,counter_d;
     PyObject *sublist;
     n = PyList_Size(data_python);
     d = PyList_Size(PyList_GetItem(data_python, 0));
-
     for (counter_n = 0; counter_n < n; counter_n++) {
         sublist = PyList_GetItem(data_python, counter_n);
         //reprint(sublist);
         for (counter_d = 0; counter_d < d; counter_d++) {
-
             //reprint(element);
             data[counter_n][counter_d] = PyFloat_AsDouble(PyList_GetItem(sublist, counter_d));
         }
     }
-
     if (PyErr_Occurred()) {
         PyErr_Print();
         err_message("Failed to convert float to double\n");
     }
 }
 
-static PyObject* calc_eigen_values_vectors(PyObject *self, PyObject *args){
+static PyObject* convert_double_float(double**data, int n) {
+    int i, j;
+    PyObject *sublist;
+    PyObject *my_list;
+    PyObject *curr;
+    my_list = PyList_New(0);
+    if(my_list == NULL) {
+        printf("error");
+    }
+    for(i = 0; i < n; i++){
+        sublist = PyList_New(0);
+        for(j = 0; j < n; j++){
+            curr = PyFloat_FromDouble(data[i][j]);
+            PyList_Append(sublist, curr);
+        }
+        PyList_Append(my_list, sublist);
+    }
+    return my_list;
+}
 
-    PyObject *data_python;
+static PyObject* calc_eigen_values_vectors(PyObject *self, PyObject *args){
+    PyObject *laplacian, *res;
     double ***ret;
     double **data;
-    int n,i,j;
-    if(!PyArg_ParseTuple(args, "Oi", &data_python, &n) || self == NULL)
+    int n;
+    if(!PyArg_ParseTuple(args, "Oi", &laplacian, &n) || self == NULL)
         return NULL;
-    if (!PyList_Check(data_python)) {
+    if (!PyList_Check(laplacian)) {
         err_message("not a list\n");
         return NULL;
     }
     data=my_alloc(n,n);
-
-
-    convert_float_double(data,data_python);
-
-
+    convert_float_double(data, laplacian);
     ret=qr_iter(data,n);
+    res = PyList_New(0);
+    PyList_Append(res, convert_double_float(ret[0], n));
+    PyList_Append(res, convert_double_float(ret[1], n));
     free_arrays(data,n);
-    //num_of_vectors=get_eigen_gap(ret[1],n);
-
-
-    return Py_BuildValue("O",ret);
+    return PySequence_List(res);
 }
 
 static PyMethodDef _eigenMethods[]={
--- main.py
+++ main.py
@@ -22,7 +22,7 @@ k = int(args.k)
 n = int(args.n)
 d = 2
 
-data = DataGen.generate_data(n, d, k)
+data = DataGen.generate_data(n, d, k) #DataGen.generate_circles(n, k)
 
 clusters = KMeans(n_clusters=k).fit(data)
 print(clusters.labels_)
@@ -37,15 +37,19 @@ plt.savefig('plot_kmeans.pdf')
 weights = GraphGen.get_weight_matrix(n, data)
 diagonal = GraphGen.get_diagonal_degree_matrix(n, weights)
 laplacian = GraphGen.get_laplacian_matrix(n, diagonal, weights)
-ret=foury.calc_eigen_values_vectors(laplacian.tolist(),n)
-print('woowwow')
+
+ret = foury.calc_eigen_values_vectors(laplacian.tolist(), n)
 
 #e_vectors, e_values_diag = qr_iter(laplacian, n)
-e_values = np.diagonal(ret[1]) #need to convert back to numpy array before this row
-k, vectors = eigen_gap_heuristic(ret[0], e_values, n)
+e_vectors = np.array(ret[1])
+e_values_diag = np.array(ret[0])
+with np.printoptions(precision=3, suppress=True):
+    print(e_values_diag)
+e_values = np.diagonal(e_values_diag) #need to convert back to numpy array before this row
+k, vectors = eigen_gap_heuristic(e_vectors, e_values, n)
 clusters = KMeans(n_clusters=k).fit(vectors.tolist())
 print(clusters.labels_)
-colors = ['r', 'g', 'b', 'y', 'c', 'm']
+colors = ['r', 'g', 'b', 'y', 'c', 'm', 'r', 'g', 'b', 'y', 'c', 'm']
 fig, ax = plt.subplots()
 for i in range(k):
     points = np.array([data[j] for j in range(len(data)) if clusters.labels_[j] == i])
