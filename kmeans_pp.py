7b1f7a52488035c09a4faa6f8886c9e2c0656ea4
yanivcogan <yanivcogan89@gmail.com>
2021-04-02 01:14:09 +0300

    optimized numpy kmeans++ implementation

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -7,20 +7,16 @@ import my_kmeans as ckmeans
     exit(0)'''
 
 
-
-
 def dist(a, b):
+    return np.sum(np.power(np.subtract(a, b), 2))
 
-    return np.sum(np.power(np.subtract(a,b),2))
 
-
-def k_means_pp(observations,k):
-    np.random.seed(0)
+def k_means_pp(observations, k):
     centroids = [np.random.choice(len(observations), 1)[0]]
     for j in range(1, k):
         dists = []
         for i in range(len(observations)):
-            holder=[]
+            holder = []
             for c in centroids:
                 holder.append(dist(observations[c], observations[i]))
             di = np.min(holder)
@@ -29,9 +25,27 @@ def k_means_pp(observations,k):
         centroids.append(np.random.choice(len(observations), p=p, size=1)[0])
     return centroids
 
-def process_pp(observations,k,n,d):
 
-    centroids = k_means_pp(observations,k)
+def k_means_pp_np(observations, k):
+    centroids = []
+    new_centroid_index = np.random.choice(len(observations), 1)[0]
+    new_centroid = observations[new_centroid_index]
+    centroids.append(new_centroid_index)
+    dists = np.power(observations - new_centroid, 2).sum(axis=1)
+    for j in range(1, k):
+        p = dists / sum(dists)
+        new_centroid_index = np.random.choice(len(observations), p=p, size=1)[0]
+        new_centroid = observations[new_centroid_index]
+        centroids.append(new_centroid_index)
+        new_dists = np.power(observations - new_centroid, 2).sum(axis=1)
+        dists = np.minimum(dists, new_dists)
+    return centroids
+
+
+def process_pp(observations, k, n, d):
+    np.random.seed(0)
+    centroids = k_means_pp_np(observations, k)
     observations = observations.tolist()
+    labels = ckmeans.k_means_api(observations, centroids, k, n, d)
+    return labels
 
-    return ckmeans.k_means_api(observations, centroids, k, n, d)
