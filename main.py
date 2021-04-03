af479a0f9babd0a0540e8eabe56a7b3727a2de47
yanivcogan <yanivcogan89@gmail.com>
2021-04-03 19:02:21 +0300

    additional comments + limit possible N, K values, cleaned code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import DataGen
 import GraphGen
 import kmeans_pp
-# from sklearn.cluster import KMeans
 import argparse
 # import time
 from Qr import *
@@ -9,7 +8,6 @@ from TextualOutput import *
 from JaccardMeasure import *
 from VisualizeResults import *
 from EigenGapSelection import *
-# import section_four as foury
 import numpy as np
 
 print("Welcome!\n")
@@ -34,27 +32,29 @@ args = parser.parse_args()
 random = args.Random
 # if in Random mode, generate random N and K values
 if random:
+    print("Random = True\n")
     n = np.random.choice(round(MAX_CAPACITY[d]["n"] / 2), 1)[0] + round(MAX_CAPACITY[d]["n"] / 2)
-    real_k = np.random.choice(round(MAX_CAPACITY[d]["k"] / 2), 1)[0] + round(MAX_CAPACITY[d]["k"] / 2)
+    real_k = min(np.random.choice(round(MAX_CAPACITY[d]["k"] / 2), 1)[0] + round(MAX_CAPACITY[d]["k"] / 2), n)
+    # real_k stores the K value used to generate the clusters
+    # k is initialized to real_k, but may later store the k value calculated via the eigen gap heuristic
+    k = real_k
 # otherwise, use the supplied N, K arguments
 else:
+    print("Random = False\n")
     n = int(args.n)
     real_k = int(args.k)
-# real_k stores the K value used to generate the clusters
-# k is initialized to real_k, but may later store the k value calculated via the eigen gap heuristic
-k = real_k
+    if real_k > n:
+        print("K must be less than N\n")
+        exit(0)
+    k = None
+print("N = "+n+"\n")
+print("K = "+real_k+"\n")
 
 # generate blob clusters
 data, cluster_designation = DataGen.generate_data(n, d, real_k)
 
 # generate concentric circles
 # data, cluster_designation = DataGen.generate_circles(n, k, d)
-
-# clusters = KMeans(n_clusters=k).fit(data)
-# print(clusters.labels_)
-
-# clusters = kmeans_pp.process_pp(data, k, n, d)
-# print(clusters)
 
 # generate weight matrix from observations
 weights = GraphGen.get_weight_matrix(n, data)
@@ -63,22 +63,13 @@ diagonal = GraphGen.get_diagonal_degree_matrix(n, weights)
 # calculate the graph laplacian
 laplacian = GraphGen.get_laplacian_matrix(n, diagonal, weights)
 
-# ret = foury.calc_eigen_values_vectors(laplacian.tolist(), n)
-# e_vectors = np.array(ret[1])
-# e_values_diag = np.array(ret[0])
-
 # use QR iteration to find eigen values/vectors
 e_vectors, e_values_diag = qr_iter(laplacian, n)
-
-# with np.printoptions(precision=3, suppress=True):
-#   print(e_values_diag)
 
 # place eigen values in a diagonal matrix
 e_values = np.diagonal(e_values_diag)
 # calculate k based on the eigen gaps, and select the first k eigen vectors
 k, vectors = eigen_gap_heuristic(e_vectors, e_values, n, k)
-
-# clusters = KMeans(n_clusters=k).fit(vectors.tolist())
 
 # calculate clusters directly on data using k-means
 labels_k_means = np.array(kmeans_pp.process_pp(data, k, n, d))
