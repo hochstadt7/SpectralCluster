e819f6d9fa095cea40f7a6abe75b4a5e13f55156
iameyalho <hochstadt@mail.tau.ac.il>
2021-04-12 21:35:05 +0300

    Updates specified un the Whatsapp

--- EigenGapSelection.py
+++ EigenGapSelection.py
@@ -2,13 +2,14 @@ import math
 import numpy as np
 
 
-def eigen_gap_heuristic(e_vectors, e_values, n, k=False):
+def eigen_gap_heuristic(e_vectors, e_values, n, k,Random):
     e_values_sort = np.argsort(e_values)
     e_vectors_sorted = np.transpose(e_vectors)[e_values_sort]
     e_values_sorted = np.sort(e_values)
     e_values_diff = np.diff(e_values_sorted)
     e_values_diff = e_values_diff[:math.ceil(int(n/2))]
-    if not k:
+
+    if(Random):
         k = np.argmax(e_values_diff) + 1
     selected_e_vectors = e_vectors_sorted[0:k]
     eigen_matrix = np.transpose(selected_e_vectors)
