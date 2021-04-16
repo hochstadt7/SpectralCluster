7903c8abd2a1220b87ee049a5d8c2297d084d2f3
yanivcogan <yanivcogan89@gmail.com>
2021-04-16 12:06:00 +0300

    prevent division by < epsilon in eigengap algorithm

--- EigenGapSelection.py
+++ EigenGapSelection.py
@@ -2,6 +2,15 @@ import math
 import numpy as np
 
 
+def normalize(x):
+    norm = np.linalg.norm(x)
+    epsilon = 0.0001
+    if abs(norm) < epsilon:
+        print("error division by zero")
+        exit(0)
+    return x / norm
+
+
 def eigen_gap_heuristic(e_vectors, e_values, n, k=False):
     e_values_sort = np.argsort(e_values)
     e_vectors_sorted = np.transpose(e_vectors)[e_values_sort]
@@ -12,6 +21,6 @@ def eigen_gap_heuristic(e_vectors, e_values, n, k=False):
         k = np.argmax(e_values_diff) + 1
     selected_e_vectors = e_vectors_sorted[0:k]
     eigen_matrix = np.transpose(selected_e_vectors)
-    norm_eigen_matrix = np.apply_along_axis(lambda x: x / np.linalg.norm(x), 1, eigen_matrix)
+    norm_eigen_matrix = np.apply_along_axis(normalize, 1, eigen_matrix)
     # print(k)
     return k, norm_eigen_matrix
